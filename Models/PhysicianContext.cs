54b87050705251da644cda48ba14b7cd058768cf
Student <example@example.com>
2020-01-21 12:18:46 -0800

    set up proj directory.

---
+++ Models/PhysicianContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Physician.Models
+{
+  public class PhysicianContext : DbContext
+  {
+    // public virtual DbSet<Doctor> Doctors { get; set; }
+    // public DbSet<Patient> Patients { get; set; }
+    // public DbSet<DoctorPatient> DoctorPatient { get; set; }
+
+    public PhysicianContext(DbContextOptions options) : base(options) { }
+  }
+}
